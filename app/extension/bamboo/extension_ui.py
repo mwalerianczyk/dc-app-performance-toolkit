75fbdb5c7baefeebfb7cfa59a6ff6a1de0a6bf25
mwalerianczyk <mwalerianczyk@atlassian.com>
2023-10-06 07:54:53 +0200

    Log in as an admin before visiting bump build number page

--- app/extension/bamboo/extension_ui.py
+++ app/extension/bamboo/extension_ui.py
@@ -18,22 +18,27 @@ def app_specific_action(webdriver, datasets):
     # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
     # just before test_2_selenium_z_log_out action
     #
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.set_credentials(username=username, password=password)
-    #         login_page.click_login_button()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
-
-    @print_timing("selenium_app_custom_action")
+    @print_timing("selenium_app_specific_user_login")
     def measure():
+        def app_specific_user_login(username='admin', password='admin'):
+            login_page = Login(webdriver)
+            login_page.delete_all_cookies()
+            login_page.go_to()
+            login_page.set_credentials(username=username, password=password)
+            login_page.click_login_button()
+        app_specific_user_login(username='admin', password='admin')
         @print_timing("selenium_app_custom_action:view_bump_build_number_page")
         def sub_measure():
             page.go_to_url(f"{BAMBOO_SETTINGS.server_url}/chain/admin/config/viewBuildNumber.action?buildKey={build_plan_id}")
             page.wait_until_visible((By.ID, "bumpBuildNumber_save"))  # Wait for summary field visible
         sub_measure()
     measure()
+
+#     @print_timing("selenium_app_custom_action")
+#     def measure():
+#         @print_timing("selenium_app_custom_action:view_bump_build_number_page")
+#         def sub_measure():
+#             page.go_to_url(f"{BAMBOO_SETTINGS.server_url}/chain/admin/config/viewBuildNumber.action?buildKey={build_plan_id}")
+#             page.wait_until_visible((By.ID, "bumpBuildNumber_save"))  # Wait for summary field visible
+#         sub_measure()
+#     measure()
